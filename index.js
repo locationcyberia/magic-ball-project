f2454733bc9dff04db604305e3c77c76e94b7708
locationcyberia <brianna.corker@protonmail.com>
2024-05-04 21:34:48 -0500

    add context to index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+console.log("Feeling indescisive? Give me a shake!");
+
+const first_name = "Cyberia";
+const last_name = "Location";
+
+let question = "Is this a question?"
+console.log("Hello, my name is " + last_name + " " + first_name + " and my question is: \n" + question);
