c8354ae8b7a683a097301e5d13cbcce76f15dc34
locationcyberia <brianna.corker@protonmail.com>
2024-05-05 12:12:44 -0500

    modify index.js

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ console.log("Feeling indescisive? Give me a shake!");
 
 const first_name = "Cyberia";
 const last_name = "Location";
+const full_name = `${first_name} ${last_name}`;
 
 let question = "Is this a question?"
-console.log("Hello, my name is " + last_name + " " + first_name + " and my question is: \n" + question);
+console.log("Hello, my name is " + full_name + " and my question is: \n" + question.toUpperCase());
