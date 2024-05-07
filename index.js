e83f659a8d92ae1191f1c5d79ff8ffb9335988df
locationcyberia <brianna.corker@protonmail.com>
2024-05-06 19:35:42 -0500

    add comments to index.js

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const lastName = "Doe";
 const fullName = `${firstName} ${lastName}`;
 
 let question = "What number did I roll?!"
-
+// prints prompt to console and following question as uppercase
 console.log(`Hello, my name is ${fullName} and my question is:\n${question.toUpperCase()}`);
 
-
+// randomly chooses a number from 1 to 8 and rounds to the nearest whole integer
 const randomNum = Math.round(Math.random() * 8) + 1;
- 
+// prints randomNum to console and returns either Odd or Even 
 console.log(`The answer to your question is ${randomNum}.\nThis number is ${randomNum % 2 === 0? 'Even' : 'Odd'}.`);
