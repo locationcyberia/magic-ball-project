c1e1885fb59ccb39140cea017f7f993c3e2948f6
locationcyberia <brianna.corker@protonmail.com>
2024-05-06 19:34:33 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 console.log("Feeling indescisive? Give me a shake!");
 
-const first_name = "Cyberia";
-const last_name = "Location";
-const full_name = `${first_name} ${last_name}`;
+const firstName = "Jane";
+const lastName = "Doe";
+const fullName = `${firstName} ${lastName}`;
 
-let question = "Is this a question?"
-console.log("Hello, my name is " + full_name + " and my question is: \n" + question.toUpperCase());
+let question = "What number did I roll?!"
+
+console.log(`Hello, my name is ${fullName} and my question is:\n${question.toUpperCase()}`);
+
+
+const randomNum = Math.round(Math.random() * 8) + 1;
+ 
+console.log(`The answer to your question is ${randomNum}.\nThis number is ${randomNum % 2 === 0? 'Even' : 'Odd'}.`);
