fe8b2ee021531293eed1d4058829c335229de3d9
locationcyberia <brianna.corker@protonmail.com>
2024-05-18 22:53:44 -0500

    modify index.js

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
-console.log("Feeling indescisive? Give me a shake!");
+// user Greeting
+const greeting = () => {
+    // prints message to console
+    console.log("Feeling indescisive? Give me a shake!");
 
-const firstName = "Jane";
-const lastName = "Doe";
-const fullName = `${firstName} ${lastName}`;
+    const firstName = "Jane";
+    const lastName = "Doe";
+    const fullName = `${firstName} ${lastName}`;
+   
+    let question = "What number did I roll?!"
+    // prints prompt to console and following question as uppercase
+    console.log(`Hello, my name is ${fullName} and my question is:\n${question.toUpperCase()}`);
+};
 
-let question = "What number did I roll?!"
-// prints prompt to console and following question as uppercase
-console.log(`Hello, my name is ${fullName} and my question is:\n${question.toUpperCase()}`);
+greeting()
 
 // randomly chooses a number from 1 to 8 and rounds to the nearest whole integer
 const randomNum = Math.round(Math.random() * 8) + 1;
 // prints randomNum to console and returns either odd or even 
 console.log(`The answer to your question is ${randomNum}.\nThis number is ${randomNum % 2 === 0? 'even' : 'odd'}.`);
+
+const app = () => {
+
+};
